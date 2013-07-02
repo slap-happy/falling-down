e3d91378d70a068c1dd21f9c8ff9ccc305426ae2
Daniel Duvall <dan@mutual.io>
2013-07-01 17:03:52 -0700

    Starting to implement tracking camera

---
+++ Assets/Behaviors/Camera/TrackingCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrackingCamera : MonoBehaviour {
+
+	public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position.Set(transform.position.x, target.position.y, transform.position.z);
+	}
+}
